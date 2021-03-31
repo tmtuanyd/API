f69ca5220e82732b7eaf546c246ebb417dd42ae8
tmtuanyd <tmtuanyd@gmail.com>
2021-03-31 18:22:40 +0700

    upadate

---
+++ src/EventSubscriber/JWTCreatedListener.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class JWTCreatedListener
+{
+    /**
+     * @param AuthenticationSuccessEvent $event
+     */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+        if($user instanceof User)
+        {
+            $data['data'] = array(
+                'roles' => $user->getRoles()
+            );
+        }
+        $event->setData($data);
+    }
+}
